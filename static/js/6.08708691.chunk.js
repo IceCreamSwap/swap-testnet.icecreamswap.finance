1eedb8552765709987c7674f41539d22d7a5c47f
icedev <78191425+icecreamdev@users.noreply.github.com>
2021-06-03 09:32:33 -0300

    release deployment

---
+++ static/js/6.08708691.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@pancakeswap/interface"]=this["webpackJsonp@pancakeswap/interface"]||[]).push([[6],{666:function(n,c){},667:function(n,c){},676:function(n,c){},677:function(n,c){},697:function(n,c){},700:function(n,c){},701:function(n,c){},706:function(n,c){},710:function(n,c){},765:function(n,c){}}]);
+//# sourceMappingURL=6.08708691.chunk.js.map
